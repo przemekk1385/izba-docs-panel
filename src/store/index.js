3433b3a9fbc7e1d27412fedbc1b25d2676e0a43e
Przemek Kaliś <przemek.kalis@gmail.com>
2021-07-26 21:51:07 +0200

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,46 @@
 import { createStore } from "vuex";
 
+import axios from "axios";
+
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    errorMessage: undefined,
+    token: undefined,
+  },
+  getters: {
+    headers: ({ token }) => ({ headers: { Authorization: `Token ${token}` } }),
+    isAuthenticated: ({ token }) => !!token,
+  },
+  mutations: {
+    errorMessage(state, message) {
+      state.errorMessage = message;
+    },
+    token(state, token) {
+      state.token = token;
+    },
+  },
+  actions: {
+    async login({ commit }, { username, password }) {
+      try {
+        const tokenPromise = await axios.post(
+          `${process.env.VUE_APP_API_HOST}/api-token-auth/`,
+          {
+            username,
+            password,
+          }
+        );
+        const { data: { token } = {} } = tokenPromise;
+        commit("token", token);
+      } catch ({ response: { data, status } }) {
+        commit("errorMessage", `Login failed, got ${status} response code.`);
+      }
+    },
+    logout({ commit }) {
+      commit("token", undefined);
+    },
+    wipeErrorMessage({ commit }) {
+      commit("errorMessage", undefined);
+    },
+  },
   modules: {},
 });
