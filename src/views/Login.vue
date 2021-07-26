73c642269d25a0d95d70185829562b0873b5ce65
Przemek Kaliś <przemek.kalis@gmail.com>
2021-07-26 21:51:27 +0200

    integrate with API

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,28 @@
 <template>
   <el-row class="row-bg" justify="center">
     <el-col :span="8">
-      <el-form label-position="right" label-width="100px">
-        <el-form-item label="Użytkownik">
-          <el-input v-model="username"></el-input>
+      <el-form
+        ref="loginForm"
+        :model="loginForm"
+        label-position="right"
+        label-width="100px"
+      >
+        <el-form-item
+          label="Username"
+          prop="username"
+          :rules="[{ required: true, message: 'Username is required.' }]"
+        >
+          <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="Hasło">
-          <el-input v-model="password" show-password></el-input>
+        <el-form-item
+          label="Password"
+          prop="password"
+          :rules="[{ required: true, message: 'Password is required.' }]"
+        >
+          <el-input v-model="loginForm.password" show-password></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">Zaloguj</el-button>
+          <el-button type="primary" @click="validateLoginForm">Login</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -17,12 +30,46 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   data() {
     return {
-      username: undefined,
-      password: undefined,
+      loginForm: {
+        username: undefined,
+        password: undefined,
+      },
     };
+  },
+  computed: {
+    ...mapGetters({ isAuthenticated: "isAuthenticated" }),
+    errorMessage() {
+      return this.$store.state.errorMessage;
+    },
+  },
+  methods: {
+    ...mapActions({ login: "login" }),
+    async submitLoginForm() {
+      const { username, password } = this;
+
+      await this.login({ username, password });
+
+      if (this.isAuthenticated) {
+        this.$router.push({ name: "List" });
+      } else {
+        this.$notify.error({
+          title: "Error",
+          message: this.errorMessage,
+        });
+      }
+    },
+    validateLoginForm() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.submitLoginForm();
+        }
+      });
+    },
   },
 };
 </script>
